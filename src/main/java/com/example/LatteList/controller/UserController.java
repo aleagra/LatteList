a537375725f5bf24cf4cb6df36d0b063024aea56
AvrilTraverso <avril.traverso9@gmail.com>
2025-06-02 16:31:48 -0300

    add CRUD user

--- src/main/java/com/example/LatteList/controller/UserController.java
+++ src/main/java/com/example/LatteList/controller/UserController.java
@@ -1,4 +1,56 @@
 package com.example.LatteList.controller;
 
+import com.example.LatteList.DTOs.UsuarioDTOs.UsuarioDetailDTO;
+import com.example.LatteList.DTOs.UsuarioDTOs.UsuarioListDTO;
+import com.example.LatteList.DTOs.UsuarioDTOs.UsuarioRequestDTO;
+import com.example.LatteList.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
 public class UserController {
+    @Autowired
+    UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<UsuarioListDTO> listar() {
+        return userService.listarUsuarios();
+    }
+    
+    @GetMapping("/{id}")
+    public UsuarioDetailDTO buscarPorId(@PathVariable Long id) {
+        return userService.buscarPorId(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<UsuarioDetailDTO> crear(
+            @Valid @RequestBody UsuarioRequestDTO req
+    ) {
+        UsuarioDetailDTO creado = userService.crearUsuario(req);
+        return ResponseEntity.status(HttpStatus.CREATED).body(creado);
+    }
+
+    @PutMapping("/{id}")
+    public UsuarioDetailDTO modificar(
+            @PathVariable Long id,
+            @Valid @RequestBody UsuarioRequestDTO req
+    ) {
+        return userService.modificarUsuario(id, req);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void eliminar(@PathVariable Long id) {
+        userService.eliminarUsuario(id);
+    }
 }
--- src/main/java/com/example/LatteList/service/UserService.java
+++ src/main/java/com/example/LatteList/service/UserService.java
@@ -1,4 +1,69 @@
 package com.example.LatteList.service;
 
+import com.example.LatteList.DTOs.UsuarioDTOs.UsuarioDetailDTO;
+import com.example.LatteList.DTOs.UsuarioDTOs.UsuarioListDTO;
+import com.example.LatteList.DTOs.UsuarioDTOs.UsuarioRequestDTO;
+import com.example.LatteList.model.Usuario;
+import com.example.LatteList.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    public UsuarioDetailDTO crearUsuario(UsuarioRequestDTO r) {
+        Usuario u = new Usuario();
+        u.setNombre(r.getNombre());
+        u.setEmail(r.getEmail());
+        u.setContrasena(r.getContrasena());
+        Usuario guardado = userRepository.save(u);
+
+        return new UsuarioDetailDTO(
+                guardado.getId(),
+                guardado.getNombre(),
+                guardado.getApellido(),
+                guardado.getEmail(),
+                guardado.getTipoDeUsuario()
+        );
+    }
+
+    public List<UsuarioListDTO> listarUsuarios() {
+        return userRepository.findAll().stream()
+                .map(u -> new UsuarioListDTO(u.getId(), u.getNombre(), u.getApellido(), u.getEmail(), u.getTipoDeUsuario()))
+                .toList();
+    }
+
+    public UsuarioDetailDTO modificarUsuario(Long id, UsuarioRequestDTO req) {
+        Usuario u = userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Visita no encontrada con ID " + id));
+        u.setNombre(req.getNombre());
+        u.setApellido(req.getApellido());
+        u.setEmail(req.getEmail());
+        u.setContrasena(req.getContrasena());
+        Usuario actualizado = userRepository.save(u);
+        return new UsuarioDetailDTO(
+                actualizado.getId(), actualizado.getNombre(), actualizado.getApellido(),
+                actualizado.getEmail(),actualizado.getTipoDeUsuario()
+        );
+    }
+
+    public void eliminarUsuario(Long id) {
+        if (!userRepository.existsById(id)) {
+            throw new EntityNotFoundException("No existe usuario con ID " + id);
+        }
+        userRepository.deleteById(id);
+    }
+
+    public UsuarioDetailDTO buscarPorId(Long id) {
+        Usuario u = userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Visita no encontrada con ID " + id));
+        return new UsuarioDetailDTO(
+                u.getId(), u.getNombre(), u.getApellido(),
+                u.getEmail(), u.getTipoDeUsuario()
+        );
+    }
 }
+
