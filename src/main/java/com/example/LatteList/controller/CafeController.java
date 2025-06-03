962e15e2c187bf9600be231a267d97dd4b6f2ec6
BlueAgus <micaelamassuco@gmail.com>
2025-06-03 14:39:16 -0300

    clases service y controller vacias

--- src/main/java/com/example/LatteList/controller/CafeController.java
+++ src/main/java/com/example/LatteList/controller/CafeController.java
@@ -1,4 +1,21 @@
 package com.example.LatteList.controller;
 
+
+import com.example.LatteList.DTOs.CafeDTOs.CafeRequestDTO;
+import com.example.LatteList.repository.CafeRepository;
+import com.example.LatteList.service.CafeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
 public class CafeController {
+
+    @Autowired
+    private CafeService service;
+
+
+
 }
--- src/main/java/com/example/LatteList/service/CafeService.java
+++ src/main/java/com/example/LatteList/service/CafeService.java
@@ -1,4 +1,14 @@
 package com.example.LatteList.service;
 
+import com.example.LatteList.repository.CafeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CafeService {
+
+    @Autowired
+    private CafeRepository repo;
+
+
 }
